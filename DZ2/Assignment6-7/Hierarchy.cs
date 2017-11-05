8d0c19351607205dd7c29f4f0a1e6a2ded3d4b69
karlomrak <32813273+karlomrak@users.noreply.github.com>
2017-11-06 00:34:52 +0100

    Add files via upload

---
+++ DZ2/Assignment6-7/Hierarchy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Assignment6_7
+{
+    public class Hierarchy
+    {
+        public static async Task LetsSayUserClickedAButtonOnGuiMethod()
+        {
+            var result =await  GetTheMagicNumber();
+            Console.WriteLine(result);
+        }
+        private static async Task<int> GetTheMagicNumber()
+        {
+            return await IKnowIGuyWhoKnowsAGuy();
+        }
+        private static async Task<int> IKnowIGuyWhoKnowsAGuy()
+        {
+            int a = await IKnowWhoKnowsThis(10);
+            int b= await IKnowWhoKnowsThis(5);
+
+            return a + b;
+        }
+        private static async Task<int> IKnowWhoKnowsThis(int n)
+        {
+            return await FactorialSum.FactorialDigitSum(n);
+        }
+    }
+}
